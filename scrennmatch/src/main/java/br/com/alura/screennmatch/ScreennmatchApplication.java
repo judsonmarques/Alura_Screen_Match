97137d3809deff22202609cacf10642cc9554b99
judsonmarquess <judsonmarquess@gmail.com>
2023-05-30 11:10:57 -0300

    Criação do Formulario.html

--- scrennmatch/src/main/java/br/com/alura/screennmatch/ScreennmatchApplication.java
+++ scrennmatch/src/main/java/br/com/alura/screennmatch/ScreennmatchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScreennmatchApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ScreennmatchApplication.class, args);
 	}
 
